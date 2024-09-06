5dc453b22b0f91245ce63e8492046611b3610ac8
Malik001-G <adisamalikng0@gmail.com>
2024-09-06 03:47:51 +0100

    ejs mate for creating layout files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 mongoose.connect("mongodb://0.0.0.0:27017/YelpCamp", {
@@ -17,6 +17,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
