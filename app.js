88ff2a11bb2bcd6d99ff83617684f49530584afe
Malik001-G <adisamalikng0@gmail.com>
2024-09-09 12:57:45 +0100

    Error template to show error messgae and stack

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    if (!req.body.campground)
+      throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -100,8 +101,9 @@ app.all("*", (req, res, next) => {
 
 //Basic Error Handler
 app.use((err, req, res, next) => {
-  const { message = "Something went wrong", statusCode = 500 } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something went wrong";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
