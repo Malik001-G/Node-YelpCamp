c2f9ea0eb17356b581433877211ad8da0ea56066
Malik001-G <adisamalikng0@gmail.com>
2024-09-09 12:45:57 +0100

    Added a 404 route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 mongoose.connect("mongodb://0.0.0.0:27017/YelpCamp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -43,6 +44,7 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -91,9 +93,15 @@ app.delete(
   })
 );
 
+// Basic 404
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
 //Basic Error Handler
 app.use((err, req, res, next) => {
-  res.send("Something went wrong");
+  const { message = "Something went wrong", statusCode = 500 } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
