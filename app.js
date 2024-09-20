5f407408d433647f25fe1d121e47f889ff19e31d
Malik001-G <adisamalikng0@gmail.com>
2024-09-20 04:38:46 +0100

    Breaking out the Reviews routes

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const { campgroundSchema, reviewSchema } = require("./schemas.js");
 const methodOverride = require("method-override");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
+
 const campgrounds = require("./routes/campgrounds.js");
+const reviews = require("./routes/reviews.js");
+
 mongoose.connect("mongodb://0.0.0.0:27017/YelpCamp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,52 +28,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join("/");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+//campground routes reference using Express Router
 app.use("/campgrounds", campgrounds);
 
-//Review Routes
-
-//Create a review
-app.post(
-  "/campgrounds/:id/reviews",
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
-//Delete a review
-app.delete(
-  "/campgrounds/:id/reviews/:reviewId",
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {
-      $pull: { reviews: reviewId },
-    });
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/campgrounds/${id}`);
-  })
-);
+//Review routes reference using Express Router
+app.use("/campgrounds/:id/reviews", reviews);
 
 // Basic 404
 app.all("*", (req, res, next) => {
