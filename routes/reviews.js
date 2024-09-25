19015f086af33d7d4da1155626f6485d431952ba
Malik001-G <adisamalikng0@gmail.com>
2024-09-25 12:17:58 +0100

    Did the controller thing for reviews and users too

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,7 @@ const express = require("express");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
+const reviews = require('../controllers/reviews')
 const {
   validateReview,
   isLoggedIn,
@@ -15,16 +16,7 @@ router.post(
   "/",
   isLoggedIn,
   validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "Created new review");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
+  catchAsync(reviews.createReview)
 );
 
 //Delete a review
@@ -32,15 +24,7 @@ router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {
-      $pull: { reviews: reviewId },
-    });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Successfully deleted a review");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
